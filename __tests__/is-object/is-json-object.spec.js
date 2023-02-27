35cd238ceea4df5649cb87c19f49f78391f8320f
jklover1110 <jklover1110@foxmail.com>
2023-02-27 10:44:51 +0800

    test(__tests__): add __tests__/is-object

---
+++ __tests__/is-object/is-json-object.spec.js
@@ -0,0 +1,24 @@
+import { suite, it, expect } from 'vitest'
+import { isJSONObject } from '@/is-object'
+
+suite('isJSONObject()', () => {
+  it('Naked Object is a JSON object', () => {
+    expect(isJSONObject(Object.create(null))).toBe(true)
+  })
+
+  it('Object.prototype is a JSON object', () => {
+    expect(isJSONObject(Object.prototype)).toBe(true)
+  })
+
+  it('[] is a JSON object', () => {
+    expect(isJSONObject([])).toBe(true)
+  })
+
+  it('Set is a JSON-compliant object', () => {
+    expect(isJSONObject(new Set())).toBe(true)
+  })
+
+  it('Function is not a JSON-compliant object', () => {
+    expect(isJSONObject(() => {})).toBe(false)
+  })
+})
---
+++ __tests__/is-object/is-object.spec.js
@@ -0,0 +1,24 @@
+import { suite, it, expect } from 'vitest'
+import { isObject } from '@/is-object'
+
+suite('isObject()', () => {
+  it('Naked Object is an object', () => {
+    expect(isObject(Object.create(null))).toBe(true)
+  })
+
+  it('Object.prototype is an object', () => {
+    expect(isObject(Object.prototype)).toBe(true)
+  })
+
+  it('[] is an object', () => {
+    expect(isObject([])).toBe(true)
+  })
+
+  it('Set is an object', () => {
+    expect(isObject(new Set())).toBe(true)
+  })
+
+  it('Function is an object', () => {
+    expect(isObject(() => {})).toBe(true)
+  })
+})
