ab665154fe2220eff3cf9b578d4d12e05495c50a
jklover1110 <jklover1110@foxmail.com>
2023-03-14 21:41:17 +0800

    test(packages/robust-undefined): add tests

--- packages/robust-undefined/__tests__/robust-undefined.spec.js
+++ packages/robust-undefined/__tests__/robust-undefined.spec.js
@@ -1,8 +1,25 @@
+/* eslint-disable no-shadow-restricted-names */
 import { suite, it, expect } from 'vitest'
 import VOID from '../index'
+
+suite('Magic undefined', () => {
+  it('Magic undefined', () => {
+    const [OOPS, undefined = OOPS, cat = undefined] = ['OOPS!']
+
+    expect(undefined).toBe(OOPS)
+
+    expect(cat).toBe(undefined)
+    expect(cat).not.toBeUndefined()
+  })
+})
 
 suite('robust undefined', () => {
   it('should be undefined', () => {
     expect(VOID).toBeUndefined()
+  })
+
+  it('globalThis.undefined', () => {
+    expect(VOID).toBeUndefined()
+    expect(VOID === globalThis.undefined).toBe(true)
   })
 })
